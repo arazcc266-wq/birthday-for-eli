e1e24d793bfc4efa9233451e5fec1d685be396d7
arazcc266-wq <araz.cc266@gmail.com>
2026-07-13 22:16:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -541,7 +541,7 @@ const flowerIcons=[
 
 ];
 
-function createFlowerRain(){
+function createFlower(){
 
 const flower=document.createElement("div");
 
@@ -571,4 +571,4 @@ flower.remove();
 
 }
 
-setInterval(createFlowerRain,350);
+setInterval(createFlower,350);
